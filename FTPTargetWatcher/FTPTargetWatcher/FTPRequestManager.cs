ae3adce23f23d4b25d0262dc7076ea8db67ad76a
DcDrugs <DcDrugs@users.noreply.github.com>
2020-11-20 21:23:22 +0300

    last update

---
+++ FTPTargetWatcher/FTPTargetWatcher/FTPRequestManager.cs
@@ -0,0 +1,124 @@
+﻿using System.ServiceProcess;
+using System.IO;
+using System.Threading;
+using System.Collections.Generic;
+using System;
+using System.Text;
+
+namespace FTPTargetWatcher
+{
+    public partial class FTPRequestManager : ServiceBase
+    {
+        private Logger _logger;
+        private string _sourcePath;
+        private string _targetPath;
+        Dictionary<ArchiveCryptMode, string> mods;
+
+        public FTPRequestManager()
+        {
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            var parser = new Parser.ParserManager();
+            parser.MakeParsed(args[0]);
+            _sourcePath = parser.SourcetPath;
+            _targetPath = parser.TargetPath;
+            if (_sourcePath == null || _targetPath == null)
+                throw new NullReferenceException("Error path");
+            mods = parser.GetMods();
+
+            _logger = new Logger(_targetPath, _sourcePath, mods);
+            Thread loggerThread = new Thread(new ThreadStart(_logger.Start));
+            loggerThread.Start();
+        }
+
+        protected override void OnStop()
+        {
+            _logger.Stop();
+            Thread.Sleep(1000);
+        }
+    }
+    public enum ArchiveCryptMode
+    {
+        Archive,
+        Dearchive,
+        Encrypt,
+        ArchiveAndEcrypt,
+        Decrypt
+    }
+
+    delegate void ProcessHandler(string directoryName);
+    delegate void AddDirectoryNameHandler(string name);
+    class Logger
+    {
+        private FileSystemWatcher _watcher;
+        private static CryptoManager _crypto = new CryptoManager();
+        private string _sourcePath;
+        private bool _enabled = true;
+        private static Options.ArchiveCryptManager _manager = new Options.ArchiveCryptManager(ref _crypto);
+        private event ProcessHandler _Processed;
+        private static readonly Dictionary<ArchiveCryptMode, AddDirectoryNameHandler> _addDirectoryName
+            = new Dictionary<ArchiveCryptMode, AddDirectoryNameHandler>
+            {
+                { ArchiveCryptMode.Archive, _manager.archivator.SetArchiveName},
+                { ArchiveCryptMode.Dearchive,  _manager.archivator.SetDearchiveName},
+                { ArchiveCryptMode.Encrypt,  _manager.cryptor.SetEncryptName },
+                { ArchiveCryptMode.ArchiveAndEcrypt,  _manager.archiveCrypt.SetArchiveAndEcryptName },
+                { ArchiveCryptMode.Decrypt, _manager.cryptor.SetDecryptName }
+            };
+        private static readonly Dictionary<ArchiveCryptMode, ProcessHandler> _operation
+            = new Dictionary<ArchiveCryptMode, ProcessHandler>
+            {
+                { ArchiveCryptMode.Archive,  _manager.ProcessCompress},
+                { ArchiveCryptMode.Dearchive,  _manager.ProcessDecompress},
+                { ArchiveCryptMode.Encrypt,  _manager.ProcessEncrypt},
+                { ArchiveCryptMode.Decrypt, _manager.ProcessDecrypt},
+                { ArchiveCryptMode.ArchiveAndEcrypt,  _manager.ProcessArchiveAndEcrypt}
+            };
+
+        public Logger(string targetPath, string sourcePath, Dictionary<ArchiveCryptMode, string> mods)
+        {
+            _sourcePath = sourcePath;
+            _watcher = new FileSystemWatcher(targetPath);
+            _watcher.Created += WatcherCreated;
+            _manager.path.MakePath(targetPath, sourcePath);
+            foreach (var mod in mods)
+            {
+                _Processed += _operation[mod.Key];
+                _addDirectoryName[mod.Key](mod.Value);
+            }
+        }
+
+        public void Start()
+        {
+            _watcher.EnableRaisingEvents = true;
+            while (_enabled)
+            {
+                Thread.Sleep(1000);
+            }
+        }
+        public void Stop()
+        {
+            _watcher.EnableRaisingEvents = false;
+            _enabled = false;
+        }
+        private void WatcherCreated(object sender, FileSystemEventArgs e)
+        {
+            try
+            {
+                var name = new Options.FileOption(_Processed, e.Name);
+                Thread loggerThread = new Thread(new ThreadStart(name.Process));
+                loggerThread.Start();
+            }
+            catch (Exception ex)
+            {
+                using (var file = new FileStream(Path.Combine(_sourcePath, e.Name + "_" + DateTime.Now + ".txt"), FileMode.Create))
+                {
+                    file.Write(Encoding.ASCII.GetBytes(ex.Message), 0, ex.Message.Length);
+                }
+            }
+        }
+
+    }
+}
---
+++ FTPTargetWatcher/FTPTargetWatcher/Parser/JSONParser.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Json
+{
+    class Parser
+    {
+        public static Document Load(StreamReader input)
+        {
+            return new Document(LoadNode(input));
+        }
+        private static Node LoadArray(StreamReader input)
+        {
+            List<Node> result = new List<Node>();
+
+            for (char c; RemoveSpace(input) && (c = Convert.ToChar(input.Peek())) != -1 && c != ']';)
+            {
+                if (c == ',')
+                {
+                    input.Read();
+                }
+                result.Add(LoadNode(input));
+            }
+
+            return new Node(result);
+        }
+
+        private static Node LoadInt(StreamReader input)
+        {
+            int result = 0;
+            while (Char.IsDigit(Convert.ToChar(input.Peek())))
+            {
+                result *= 10;
+                result += input.Read() - 48;
+            }
+            return new Node(result);
+        }
+
+        private static string Separated(StreamReader input, char by = '\0')
+        {
+            var line = string.Empty;
+            while (input.Peek() >= 0)
+            {
+                char c = (char)input.Read();
+                if (c == '\n' || c == by || c== '\r')
+                {
+                    return line;
+                }
+                else
+                {
+                    line += c;
+                }
+            }
+            return line;
+        }
+
+        private static Node LoadString(StreamReader input)
+        {
+            string line;
+            line = Separated(input, '"');
+            return new Node(line);
+        }
+
+        private static Node LoadDict(StreamReader input)
+        {
+            var result = new Dictionary<string, Node>();
+
+
+            for (char c; RemoveSpace(input) && (c = Convert.ToChar(input.Read())) != -1 && c != '}';)
+            {
+                if (c == ',')
+                {
+                    RemoveSpace(input);
+                    c = Convert.ToChar(input.Read());
+                }
+
+                string key = LoadString(input).AsString();
+                input.Read();
+                result.Add(key, LoadNode(input));
+            }
+
+            return new Node(result);
+        }
+
+        private static bool RemoveSpace(StreamReader input)
+        {
+            while (input.Peek() == 32 || input.Peek() == '\r' || input.Peek() == '\n') input.Read();
+            return true;
+        }
+
+        private static Node LoadNode(StreamReader input)
+        {
+            RemoveSpace(input);
+            char c = Convert.ToChar(input.Peek());
+
+            switch (c)
+            {
+                case '[':
+                    {
+                        input.Read();
+                        RemoveSpace(input);
+                        return LoadArray(input);
+                    }
+                case '{':
+                    {
+                        input.Read();
+                        RemoveSpace(input);
+                        return LoadDict(input);
+                    }
+                case '"':
+                    {
+                        input.Read();
+                        RemoveSpace(input);
+                        return LoadString(input);
+                    }
+                default:
+                    {
+                        return LoadInt(input);
+                    }
+            }
+        }
+    }
+
+    class Node
+    {
+        private readonly Object _data;
+        public Node(Object data)
+        {
+            _data = data;
+        }
+
+        public List<Node> AsArray()
+        {
+            return (List<Node>)Convert.ChangeType(_data, typeof(List<Node>));
+        }
+
+        public Dictionary<string, Node> AsDictionary()
+        {
+            return (Dictionary<string, Node>)Convert.ChangeType(_data, typeof(Dictionary<string, Node>));
+        }
+        public int AsInt()
+        {
+            return (int)Convert.ChangeType(_data, typeof(int));
+        }
+        public string AsString()
+        {
+            return (string)Convert.ChangeType(_data, typeof(string));
+        }
+    };
+
+    class Document
+    {
+        private readonly Node _root;
+        public Document(Node root)
+        {
+            _root = root;
+        }
+
+        public Node GetRoot()
+        {
+            return _root;
+        }
+    };
+}
